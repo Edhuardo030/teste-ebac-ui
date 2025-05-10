03508a56b3dba3fedd6e4204b036a78f6b419aaf
Edhuardo030 <edhuardo030@gmail.com>
2025-05-09 21:26:16 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,13 +1,39 @@
 /// <reference types="cypress"/>
 
 describe('Funcionalidade: login', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')     
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
     
     it('Deve fazer login com sucesso', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    cy.get('#username').type('francisco10159@gmail.com')
-    cy.get('#password').type('N@s210601')
-    cy.get('#rememberme').click()
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, francisco10159 (não é francisco10159? Sair)')   
+        cy.get('#username').type('francisco10159@gmail.com')
+        cy.get('#password').type('N@s210601')
+        cy.get('#rememberme').click()
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, francisco10159 (não é francisco10159? Sair)')   
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
+        cy.get('#username').type('francisco@gmail.com')
+        cy.get('#password').type('N@s')
+        cy.get('#rememberme').click()
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+        cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('francisco10159@gmail.com')
+        cy.get('#password').type('N@s')
+        cy.get('#rememberme').click()
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail francisco10159@gmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+    });
+
 })
