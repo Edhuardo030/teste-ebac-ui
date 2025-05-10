1df1a1195203dd920a4983def7d92a1c50218a61
Edhuardo030 <edhuardo030@gmail.com>
2025-05-09 21:33:35 -0300

    ajuste de cenário

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -11,8 +11,8 @@ describe('Funcionalidade: login', () => {
     });
     
     it('Deve fazer login com sucesso', () => {
-        cy.get('#username').type('francisco10159@gmail.com')
-        cy.get('#password').type('N@s210601')
+        cy.get('#username').type('eduardo030@gmail.com')
+        cy.get('#password').type('@teste123')
         cy.get('#rememberme').click()
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, francisco10159 (não é francisco10159? Sair)')   
